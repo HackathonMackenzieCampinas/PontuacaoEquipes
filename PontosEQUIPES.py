88f9e47bfa57e0326a84846abeac275a34aa3e4c
HackathonMackenzieCampinas <149341593+HackathonMackenzieCampinas@users.noreply.github.com>
2023-11-19 17:42:40 -0300

    Update PontosEQUIPES.py

--- PontosEQUIPES.py
+++ PontosEQUIPES.py
@@ -89,7 +89,7 @@ if len(dfP) != 0:
     st.dataframe(DF.sort_values(by='Nota', ascending=False))     
   with colNotas3:
     st.title(" ")
-    st.image(image02, width=150, caption='Classificação Final')
+    st.image(image02, width=150, caption='Fonte: https://hackathonmackenziecampinas-gestaoequipes.streamlit.app')
     st.markdown("<h1 style='text-align: justify; font-family:arial; font-size: 14px; color: gray;'>Este painel tem o objetivo de auxiliar a Equipe de Gestão do Hackathon 2023 escolher as 5 equipes finalistas que participarão do Evento Final, disputando uma das 3 vagas na dinâmica Elevator Pitch.</h1>", unsafe_allow_html=True)
     st.markdown("<h1 style='text-align: left; color: blue; font-size: 20px'>Ranking de Citação nas Avaliações</h1>", unsafe_allow_html=True)
     st.write(dfP["Equipe"].value_counts())  
